8f01a0549d936e48039d09343e6ae5d3153fd779
ConstanzeDev <julius.nieves@sascholar.org>
2020-08-27 22:24:49 -0400

    Update setplaying.js

--- commands/setplaying.js
+++ commands/setplaying.js
@@ -5,20 +5,26 @@ module.exports = {
   description: 'Set Playing Status of Ava',
   type: 'Private',
   aliases: ['setstatus'],
-  usage: '<status> <activity_type> <activity_name> <activity_url>',
-  execute(client, msg, args) {
+  usage: '<status> <activity_type> <activity_url> <activity_name>',
+  example: 'online streaming https://twitch.tv/julztdg cod mobile',
+  async execute(client, msg, args) {
     const allowedStatuses = ['online', 'idle', 'invisible', 'dnd', 'offline']
-    if(allowedStatuses.includes(args[0]))
+    if(!allowedStatuses.includes(args[0])) return
     const allowedTypes = ['LISTENING', 'STREAMING', 'PLAYING', 'WATCHING']
-    if(allowedTypes.includes(args[1].toUpperCase()))
+    if(!allowedTypes.includes(args[1].toUpperCase())) return
+    let xrgs = msg.content.slice(prefix.length).split(/\s+/)
+    xrgs.shift()
+    xrgs.shift()
+    xrgs.shift()
+    args[3] = xrgs.join(' ')
     client.user.setPresence({
       activity: {
-        name: args[2],
+        name: args[3],
         type: args[1].toUpperCase(),
-        url: args[3]
+        url: args[2]
       },
       status: args[0] == 'offline' ? 'invisible' : args[0]
     })
-    msg.channel.send(`\`\`\`asciidoc\nStatus::${' '.repeat(5)}${args[0]}\nActivity Name::${' '.repeat(5)}${args[2]}\nActivity Type::${' '.repeat(5)}${args[1].toUpperCase()}\nActivity URL::${' '.repeat(5)}${args[3]}\n\`\`\``)
+    msg.channel.send(`\`\`\`asciidoc\nStatus::${' '.repeat(5)}${args[0]}\nActivity Name::${' '.repeat(5)}${args[3]}\nActivity Type::${' '.repeat(5)}${args[1].toUpperCase()}\nActivity URL::${' '.repeat(5)}${args[2]}\n\`\`\``)
   }
 }
